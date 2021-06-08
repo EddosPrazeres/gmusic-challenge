c2412b99b8b45fcc3509cfa7a0ab53728f9b9c00
EddosPrazeres <edimilsonpereira.pro@gmail.com>
2021-06-08 19:02:35 -0300

    Refactor on legacy codes and update structure for routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import * as views from "../views";
+import { Paths } from './Paths';
+
+interface IState {
+  isSmallScreen: boolean;
+}
+
+class Routes extends React.Component<{}, IState> {
+  constructor(props : any){
+    super(props);
+
+    this.state = { isSmallScreen: false };
+}
+  public render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+            <Route exact={true} path={Paths.HOME} component={views.HomeView}/>
+            <Route path={Paths.DETAILS} component={views.DetailsView}/>
+            <Redirect to={Paths.HOME} />
+          </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Routes;
