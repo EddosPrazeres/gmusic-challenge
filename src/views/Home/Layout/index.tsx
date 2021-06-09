287656a4c4d298a16808ed0e1db1b5fe306f309d
EddosPrazeres <edimilsonpereira.pro@gmail.com>
2021-06-09 11:11:32 -0300

    In Progress Home using Atomic Text

--- src/views/Home/Layout/index.tsx
+++ src/views/Home/Layout/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
+import { Text } from "../../../components";
 import { IHomeLayout } from "../data";
-// import './styles.scss'
+import './styles.scss'
 
 export default function HomeView({ title= "HomeView" }: IHomeLayout) {
   return (
     <div className="HomeView">
+        <Text title="HomeView"/>
       {title}
     </div>
   );
