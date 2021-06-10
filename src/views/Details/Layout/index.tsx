35d7cfde958f3381b62d206d9e78d6a35cc6a3fb
EddosPrazeres <edimilsonpereira.pro@gmail.com>
2021-06-09 22:01:08 -0300

    Update Details View for using DesignSystem and Atomic Design Components

--- src/views/Details/Layout/index.tsx
+++ src/views/Details/Layout/index.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
-import { IDetailsLayout } from "../data";
-// import './styles.scss'
+import { IDetailsLayout, JSONUsers } from "../data";
+import { CardUserName, Header, CardDetails, CardWrapper, Text, CardElement, ListWrapper, RepositoriesList } from '../../../components'
+import './styles.scss'
 
 export default function DetailsView({ title= "DetailsView" }: IDetailsLayout) {
   return (
     <div className="DetailsView">
-      {title}
+      <Header title="Detalhes de usuário do Github" hasBack/> 
+      <CardUserName {...JSONUsers[0]} />
+      <CardDetails />
+      <CardDetails title="Repositorios"/>
+      <RepositoriesList ListElements={[0,1,2,3,4,5,6,7,8,9]} />
     </div>
   );
 }
