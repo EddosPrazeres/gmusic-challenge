42958286bb4b5dc3ea3bbeb52d840cc0c1cda2ff
EddosPrazeres <edimilsonpereira.pro@gmail.com>
2021-06-11 12:12:49 -0300

    Update test ancor in progress

--- src/components/Base/Ancor/__test__/index.test.tsx
+++ src/components/Base/Ancor/__test__/index.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { mount, shallow } from 'enzyme'
 import Ancor from '../index'
+import { Ancor as Layout } from '../Layout'
+
+const mockedProps = {
+  title: "Título",
+  target: "_blank",
+  link: "wwww.google.com",
+  onClick: jest.fn(),
+}
 
 const createComponent = (props, useDefault = false) => {
-  const componentProps = {    
+  const componentProps = {   
+    ...mockedProps,
     ...props,
   }
 
@@ -20,4 +29,20 @@ describe('Ancor', () => {
     expect(wrapper).toMatchSnapshot()
     unmount()
   })
+
+  describe('Props', () => {
+    it('should call Layout Component with all Menu Component Ancor', () => {
+      const [wrapper, unmount] = createComponent({})
+      
+      expect(wrapper.find(Layout).props()).toEqual(mockedProps)      
+      unmount()
+    })
+
+    it('should call Layout Component with all Menu Component Ancor', () => {
+      const [wrapper, unmount] = createComponent({title: undefined})
+      
+      expect(wrapper.find(Layout).props().title).toBeDefined()
+      unmount()
+    })
+  })  
 })
